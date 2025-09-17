a16de52495aca8c745dce810a07939a5d4ab84d9
ArabellaSajuela-UCM <sajuelaac@gmail.com>
2025-09-17 22:03:47 +0800

    Chapter_1 exercises added

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,14 @@
+public class Comments {
+
+public static void main (String[] args) {
+
+System.out.println("//This is a single-line comment.");
+System.out.println();
+System.out.println("/*This" + "\n      is" + "\n        a" + "\n         multi-line" + "\n                   comment.*/");
+System.out.println();
+System.out.println("/**And, this is a Javadoc comment which uses API documentation using the javadoc tool that" + 
+"\n includes special tags to describe elements such as parameters which is tagged as @param.*/");
+
+   }
+
+}

a51c072f901f7096081a195ebbd4c8e1158e88b5
Arabella Sajuela <sajuelaac@gmail.com>
2025-09-17 21:12:31 +0800

    Add files via upload

---
+++ FavouriteSong.java
@@ -0,0 +1,10 @@
+public class FavouriteSong {
+
+public static void main(String[] args) {
+
+System.out.println("I'm still alive, but I'm barely breathing,");
+System.out.println("Just prayed to a God that I don't believe in,");
+System.out.println("'Cause I got time while she got freedom,");
+System.out.println("'Cause when a heart breaks, no, it don't breakeven..");
+   }
+}
